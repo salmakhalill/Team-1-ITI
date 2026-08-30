76e733731fa1b863d2590568c836d194d56a5718
AbdoEmad68 <abdelrahmane796@gmail.com>
2026-08-30 16:21:54 +0300

    feat: setup core database models and initial migration

---
+++ Models/Supplier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace Team_1_ITI.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+
+        public string? ContactName { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+
+        public List<Purchase> Purchases { get; set; } 
+    }
+}
