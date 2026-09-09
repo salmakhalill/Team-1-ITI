e1e7261d9113b01324f277ebfdde8c56534555c4
salmakhalill <salmakhalil283@gmail.com>
2026-09-09 16:45:11 +0300

    feat(products): standardize UI layout and align with global design system

---
+++ ViewModels/Products/ProductIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Team_1_ITI.ViewModels.Products
+{
+    public class ProductIndexViewModel
+    {
+        public List<ProductViewModel> Products { get; set; } = new();
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public string CurrentSearch { get; set; } = "";
+        public int? CurrentCategory { get; set; }
+        public string CurrentStatus { get; set; } = "";
+    }
+}
